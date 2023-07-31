6ade0d9475d6b0fe5881a377ae7fee2a2f08e6f5
Svetlana Martianova <smartianova8@mail.ru>
2023-08-01 00:39:26 +0400

    backend: valid

--- app.js
+++ app.js
@@ -12,7 +12,29 @@ const { limiter } = require('./limiter');
 const { errorHandler } = require('./errors/errorHandler');
 
 const app = express();
+require('dotenv').config();
 
+const { NODE_ENV, JWT_SECRET, BASE_URL } = process.env;
+console.log(NODE_ENV, JWT_SECRET, BASE_URL);
+mongoose.connect(BASE_URL, {
+  useNewUrlParser: true,
+}).then(() => { console.log('connected db'); });
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+app.use(requestLogger);
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(limiter);
+
+routes(app);
+app.use(errors());
+app.use(errorLogger);
+app.use(errorHandler);
 
 app.listen(3003, () => {
   console.log('server is running');
