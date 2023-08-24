b77dab5f045ef854b766b03a5a18c889757ff639
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 22:05:50 +0400

    react+js

--- app.js
+++ app.js
@@ -13,7 +13,22 @@ const { errorHandler } = require('./errors/errorHandler');
 
 const app = express();
 require('dotenv').config();
+app.use(function(req, res, next) {
+   // Сохраняем источник запроса в переменную origin
+  console.log('origin', req.headers)
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3003');
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+// Если это предварительный запрос, добавляем нужные заголовки
+if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+}
 
+  next();
+});
 
 const { NODE_ENV, JWT_SECRET, BASE_URL } = process.env;
 console.log(NODE_ENV, JWT_SECRET, BASE_URL);
@@ -24,7 +39,7 @@ app.use(
   cors({
     origin: ['http://sveta.movies-explorer.nomoredomainsicu.ru', 'https://sveta.movies-explorer.nomoredomainsicu.ru'],
     allowedHeaders: ['Content-Type', 'origin', 'Accept', 'Set-Cookie'],
-  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH', "OPTIONS"],
     credentials: true,
   }),
 );
