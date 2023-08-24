f460b56fb753c34e235e59f73e98acf914d24b1f
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 20:56:00 +0400

    react+js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 app.use(function(req, res, next) {
    // Сохраняем источник запроса в переменную origin
   console.log('origin', req.headers)
-  res.header('Access-Control-Allow-Origin', 'http://sveta.movies-explorer.nomoredomainsicu.ru');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3003');
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   const { method } = req;
 // Если это предварительный запрос, добавляем нужные заголовки
