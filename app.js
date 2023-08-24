4b8ad8ed6b88c94f85ce412d61c8df81023611b3
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 17:48:42 +0400

    react+js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   res.header({"Access-Control-Allow-Origin": origin});
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
