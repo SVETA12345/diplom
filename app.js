abfeb9d9486fd9a6f4313b15ae8fac195e0c759d
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 18:12:10 +0400

    react+js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ mongoose.connect(BASE_URL, {
 }).then(() => { console.log('connected db'); });
 app.use(
   cors({
-      origin: ['http://localhost:3003'],
+      origin: ['http://localhost:3003', 'http://api.movies-explorer.nomoreparties.co', 'https://api.movies-explorer.nomoreparties.co'],
       allowedHeaders: ['Content-Type', 'origin', 'Accept', 'Set-Cookie'],
   methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
   preflightContinue: false,
