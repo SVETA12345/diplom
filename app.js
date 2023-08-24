74eea73ba08d79a36c5cbe9b3414abb897c710e7
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 17:42:03 +0400

    react+js

--- app.js
+++ app.js
@@ -14,7 +14,21 @@ const { errorHandler } = require('./errors/errorHandler');
 const app = express();
 require('dotenv').config();
 app.use((req, res, next) => {
-  res.header({"Access-Control-Allow-Origin": "*"});
+  const { origin } = req.headers;
+  res.header({"Access-Control-Allow-Origin": origin});
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+const requestHeaders = req.headers['access-control-request-headers'];
+// Если это предварительный запрос, добавляем нужные заголовки
+if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+}
   next();
 })
 app.use(
