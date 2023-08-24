28735ebab3e3e782d78607553e7d127221ec31b7
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 18:01:52 +0400

    react+js

--- app.js
+++ app.js
@@ -13,45 +13,19 @@ const { errorHandler } = require('./errors/errorHandler');
 
 const app = express();
 require('dotenv').config();
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  res.header({"Access-Control-Allow-Origin": origin});
-  res.setHeader('Access-Control-Allow-Credentials', true);
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-const requestHeaders = req.headers['access-control-request-headers'];
-// Если это предварительный запрос, добавляем нужные заголовки
-if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-}
-  next();
-})
-app.use(
-  cors({
-      origin: [
-        'http://api.movies-explorer.nomoreparties.co',
-        'https://api.movies-explorer.nomoreparties.co',
-        'http://localhost:3003',
-        'https://localhost:3003'
-      ],
-      credentials: true, // Если используются куки.
-  }),
-);
 
 const { NODE_ENV, JWT_SECRET, BASE_URL } = process.env;
 console.log(NODE_ENV, JWT_SECRET, BASE_URL);
 mongoose.connect(BASE_URL, {
   useNewUrlParser: true,
 }).then(() => { console.log('connected db'); });
+app.use(
+  cors({
+      origin: true,
+      credentials: true, // Если используются куки.
+  }),
+);
 
 app.use(cookieParser());
 app.use(requestLogger);
