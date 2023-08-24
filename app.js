2cba9e0053501ca2d0115a476eb7fade4cf1148b
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 17:05:45 +0400

    react+js

--- app.js
+++ app.js
@@ -14,6 +14,27 @@ const { errorHandler } = require('./errors/errorHandler');
 const app = express();
 require('dotenv').config();
 
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  res.header('Access-Control-Allow-Origin', origin);
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+const requestHeaders = req.headers['access-control-request-headers'];
+// Если это предварительный запрос, добавляем нужные заголовки
+if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+}
+
+  next();
+});
 const { NODE_ENV, JWT_SECRET, BASE_URL } = process.env;
 console.log(NODE_ENV, JWT_SECRET, BASE_URL);
 mongoose.connect(BASE_URL, {
