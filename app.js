30acb532141b4e53c0aa20ee167dde81de6d32a3
Svetlana Martianova <smartianova8@mail.ru>
2023-08-24 21:23:28 +0400

    react+js

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(function(req, res, next) {
 // Если это предварительный запрос, добавляем нужные заголовки
 if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Request-Method', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Request-Headers', "Content-Type");
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', "Content-Type");
 }
 
   next();
