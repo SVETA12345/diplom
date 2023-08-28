2f61f1e8bdc329a49bde06f27691ea4bfdd78e94
Svetlana Martianova <smartianova8@mail.ru>
2023-08-28 16:31:18 +0400

    react+js

--- controllers/users.js
+++ controllers/users.js
@@ -100,7 +100,7 @@ const login = (req, res, next) => {
           res.status(200).cookie('jwt', token, {
             maxAge: 6400000,
             httpOnly: true,
-            sameSite: true,
+            sameSite: 'None',
             secure: true,
             domain: ".movies-explorer.nomoreparties.co"
           });
